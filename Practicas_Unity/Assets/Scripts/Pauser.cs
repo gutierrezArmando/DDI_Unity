7c23b6739cbd77a078d94d034cd06710a74fcd4e
armandoGutierrez <gutierrezjarr@gmail.com>
2018-08-24 19:45:22 -0700

    Mitad del menu implementado

--- Practicas_Unity/Assets/Scripts/Pauser.cs
+++ Practicas_Unity/Assets/Scripts/Pauser.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;/*Para utilizar el metodo LoadScene */
 
-public class Pauser : MonoBehaviour {
+public class Pauser : MonoBehaviour
+{
 	private bool paused = false;
+    public GameObject pausePanel;
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if(Input.GetKeyUp(KeyCode.P))
 		{
 			paused = !paused;
+            pausePanel.SetActive(paused);
 		}
 
 		if(paused)
@@ -16,4 +21,23 @@ public class Pauser : MonoBehaviour {
 		else
 			Time.timeScale = 1;
 	}
+
+    public void ContinueGame()
+    {
+        Debug.Log("Continuar...");
+        paused = false;
+        pausePanel.SetActive(paused);
+    }
+
+    public void ResetGame()
+    {
+        Debug.Log("Reset...");
+        SceneManager.LoadScene(0);
+    }
+
+    public void MuteMusic()
+    {
+        Debug.Log("Mute...");
+    }
+
 }
