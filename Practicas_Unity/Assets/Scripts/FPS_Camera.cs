f97769a05098d1acfe3716a4fe3db517933ea236
armandoGutierrez <gutierrezjarr@gmail.com>
2018-09-06 19:47:35 -0700

    Clase 6-Agosto-2018
    Se agrega Cubo interactivo

---
+++ Practicas_Unity/Assets/Scripts/FPS_Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Camera : MonoBehaviour {
+
+    public Camera FPSCamera;
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    float h;
+    float v;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = horizontalSpeed * Input.GetAxis("Mouse X");
+        v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0,h,0);
+	}
+}
